e29aa6ad9bb2556e70c1c7217dac0dbfe13ffdb5
lautaro-luna <128639299+lau-luna@users.noreply.github.com>
2024-09-23 23:32:15 -0300

    Created Diary page

--- js/spring-2022-photo-loader.js
+++ js/spring-2022-photo-loader.js
@@ -13,22 +13,43 @@ let links =  ['https://files.catbox.moe/itfr9i.jpg',
     'https://files.catbox.moe/kod1oo.JPG',
     'https://files.catbox.moe/wjpw36.JPG',
     'https://files.catbox.moe/ge3w5n.JPG',
-    'https://files.catbox.moe/svqsgh.JPG',
-    'https://files.catbox.moe/1dxsfp.JPG',
-    'https://files.catbox.moe/igts1l.JPG',
-    'https://files.catbox.moe/m2yzn8.JPG',
     'https://files.catbox.moe/203hep.JPG'
 ];
 
 function loadImages() {
-    let images = document.getElementsByClassName('images');
-
+    let imagesContainer = document.getElementsByClassName('images')[0];
+  
     links.forEach(link => {
-        let img = document.createElement('img');
-        img.src = link;
-        img.alt = '';
-        images[0].appendChild(img);
+      let img = document.createElement('img');
+      img.src = link;
+      img.alt = '';
+      img.className = 'image'; // Agregamos una clase a la imagen
+  
+      // Agregamos un evento de clic a la imagen
+      img.addEventListener('click', function() {
+        // Creamos un elemento de diálogo (lightbox)
+        let lightbox = document.createElement('div');
+        lightbox.className = 'lightbox';
+  
+        // Creamos un elemento de imagen dentro del diálogo
+        let lightboxImg = document.createElement('img');
+        lightboxImg.src = this.src; // Utilizamos la fuente de la imagen actual
+        lightboxImg.alt = '';
+  
+        // Agregamos la imagen al diálogo
+        lightbox.appendChild(lightboxImg);
+  
+        // Agregamos el diálogo al cuerpo del documento
+        document.body.appendChild(lightbox);
+  
+        // Agregamos un evento de clic al diálogo para cerrarlo
+        lightbox.addEventListener('click', function() {
+          this.remove();
+        });
+      });
+  
+      imagesContainer.appendChild(img);
     });
-}
+  }
 
 loadImages();
